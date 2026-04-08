5d96a36e87a64e6083e336dfdb5d7e621f0bffa7
mrgong0515 <mrgong0515@gmail.com>
2026-04-08 17:18:10 +0900

    comment upadte

--- starter/main.c
+++ starter/main.c
@@ -14,10 +14,10 @@ void Ready_State(void);
 
 // ---------------------------------
 // 패널 번호 기준
-// 4번 패널 = 맨위    = module 0
-// 3번 패널 = 그 아래 = module 1
-// 2번 패널 = 그 아래 = module 2
-// 1번 패널 = 맨아래 = module 3
+// 4번 패널 = 맨 위    = module 0
+// 3번 패널 = 그 아래  = module 1
+// 2번 패널 = 그 아래  = module 2
+// 1번 패널 = 맨 아래  = module 3
 // ---------------------------------
 
 unsigned char num_3[8] = {
@@ -32,17 +32,15 @@ unsigned char num_1[8] = {
     0x00, 0x01, 0x01, 0xFF, 0x41, 0x21, 0x00, 0x00
 };
 
-// ---------------------------
 // 세로 표시용 문자 패턴
 // 위에서 아래로
-// F
-// S
-// P
-// 1 / 2
-// 이렇게 보이도록 회전한 비트맵
-// ---------------------------
+// F            -> False
+// S            -> Start
+// P            -> Player
+// 1 or 2
+// 이렇게 보이도록 회전한 비트맵 (가로 비트맵이라서 세로로 회전 필요)
 
-// F : 오른쪽으로 90도 두 번 돌린 형태(=180도 회전)
+// F : 왼쪽 90도 회전
 unsigned char char_F[8] = {
     0xC0,
     0xD0,
@@ -54,7 +52,7 @@ unsigned char char_F[8] = {
     0xFF
 };
 
-// S : 오른쪽으로 90도 두 번 돌린 형태(=180도 회전)
+// S : 오른쪽 90도 두 번 돌린 형태(=180도 회전)
 unsigned char char_S[8] = {
     0x4C,
     0x92,
@@ -78,7 +76,7 @@ unsigned char char_P[8] = {
     0xFE
 };
 
-// X
+// X : 0 ~ 3 패널 전체 X자 표시
 unsigned char char_X[8] = {
     0x81,   // 10000001
     0x42,   // 01000010
@@ -90,6 +88,7 @@ unsigned char char_X[8] = {
     0x81    // 10000001
 };
 
+// 대기 상태에서 양쪽 차량이 정위치에 있는지 확인 및 1번과 2번 패널 깜빡이는 효과로 "대기" 상태 표시
 void Ready_State(void)
 {
     for (int i = 0; i < 5; i++) {
@@ -103,11 +102,11 @@ void Ready_State(void)
     }
 }
 
-// 0: 정상, 1: P1 부정, 2: P2 부정, 3: 둘 다 부정
+// [0]: 정상 출발, [1]: P1 부정 출발, [2]: P2 부정 출발, [3]: 둘 다 부정 출발
 int Check_False_Start_Detail(unsigned int threshold)
 {
     int s1 = (Adc_Read(0) > threshold); // PA0가 밝아지면 1
-    int s2 = (Adc_Read(1) > threshold); // PA1이 밝아지면 1
+    int s2 = (Adc_Read(1) > threshold); // PA1이 밝아지면 2
 
     if (s1 && s2) return 3; // 둘 다 출발
     if (s1) return 1;       // Player 1만 출발
@@ -119,7 +118,7 @@ int Countdown_State(unsigned int threshold)
 {
     int fs = 0;
 
-    // --- 숫자 3 ---
+    // --- 숫자 3 --- //
     MAX7219_ClearAll();
     MAX7219_FillModule(2);
     MAX7219_ShowPattern(3, num_3);
@@ -131,7 +130,7 @@ int Countdown_State(unsigned int threshold)
         TIM2_Delay(100);
     }
 
-    // --- 숫자 2 ---
+    // --- 숫자 2 --- //
     MAX7219_ClearAll();
     MAX7219_FillModule(1);
     MAX7219_ShowPattern(3, num_2);
@@ -143,7 +142,7 @@ int Countdown_State(unsigned int threshold)
         TIM2_Delay(100);
     }
 
-    // --- 숫자 1 ---
+    // --- 숫자 1 --- //
     MAX7219_ClearAll();
     MAX7219_FillModule(0);
     MAX7219_ShowPattern(3, num_1);
@@ -155,7 +154,7 @@ int Countdown_State(unsigned int threshold)
         TIM2_Delay(15);
     }
 
-    // --- 전체 ON (GO 직전 찰나의 대기) ---
+    // --- 전체 ON (직전 잠시 대기 딜레이) --- //
     MAX7219_FillModulesRange(0, 3);
 
     for (int i = 0; i < 10; i++) {
@@ -164,11 +163,11 @@ int Countdown_State(unsigned int threshold)
         TIM2_Delay(100);
     }
 
-    // --- 전체 OFF 및 시작 ---
+    // --- 전체 OFF 및 시작 --- //
     MAX7219_ClearAll();
     Buzzer_Start_Race();
 
-    return 0; // 정상 출발 완료
+    return 0;       // 정상 출발 완료
 }
 
 void Adc_Init(void)
@@ -190,10 +189,10 @@ void Adc_Init(void)
 
 unsigned int Adc_Read(int channel)
 {
-    ADC1->SQR3 = channel;          // 읽을 채널 선택
-    ADC1->CR2 |= (1 << 30);        // SWSTART: 변환 시작
-    while (!(ADC1->SR & (1 << 1))); // EOC 대기
-    return ADC1->DR;               // 결과값 반환
+    ADC1->SQR3 = channel;               // 읽을 채널 선택
+    ADC1->CR2 |= (1 << 30);             // SWSTART: 변환 시작
+    while (!(ADC1->SR & (1 << 1)));     // EOC 대기
+    return ADC1->DR;                    // 결과값 반환
 }
 
 void Sys_Init(int baud)
@@ -205,8 +204,8 @@ void Sys_Init(int baud)
     RCC->CFGR = 0;
 
     // 하드웨어 초기화
-    Uart2_Init(baud); // 디버그용
-    Uart1_Init(baud); // HC-05 블루투스용
+    Uart2_Init(baud);                   // 디버그용
+    Uart1_Init(baud);                   // HC-05 블루투스용
     setvbuf(stdout, NULL, _IONBF, 0);
 }
 
@@ -228,15 +227,15 @@ void Main(void)
     printf("Racing Track Start System Initialized...\n");
 
     while (1) {
-        sensor1 = Adc_Read(0); // PA0
-        sensor2 = Adc_Read(1); // PA1
+        sensor1 = Adc_Read(0);      // PA0
+        sensor2 = Adc_Read(1);      // PA1
 
         printf("S1: %d | S2: %d\n", sensor1, sensor2);
 
         // 1. 차량 정위치 확인 (조도 센서 둘 다 어두움)
         if (sensor1 < threshold && sensor2 < threshold) {
 
-            // 차량 감지 시 "대기" 상태 표시
+            // 차량 감지 시 "대기" 상태
             Ready_State();
 
             // 2. 버튼 눌림 확인
@@ -246,7 +245,7 @@ void Main(void)
                 // 버튼에서 손을 뗄 때까지 대기
                 Key_Wait_Key_Released();
 
-                // 3. 카운트다운 및 레이스 시작
+                // 3. 카운트다운 및 레이스 시작 (출발)
                 int result = Countdown_State(threshold);
 
                 if (result > 0) {
@@ -255,9 +254,10 @@ void Main(void)
                         Buzzer_Count_Short();
                     }
 
-                    // 2. 상황에 맞는 메시지 출력
+                    // 2. 패널에 상황에 맞는 메시지 출력
                     MAX7219_ClearAll();
 
+                    // 2-1. P1 부정 출발
                     if(result == 1) {
                     printf("FSP1 Detected!\n");
                     MAX7219_ShowPattern(0, char_F);
@@ -266,6 +266,7 @@ void Main(void)
                     MAX7219_ShowPattern(3, num_1);
                     }
 
+                    // 2-2. P2 부정 출발
                     else if(result == 2) {
                     printf("FSP2 Detected!\n");
                     MAX7219_ShowPattern(0, char_F);
@@ -274,6 +275,7 @@ void Main(void)
                     MAX7219_ShowPattern(3, num_2);
                     }
                     
+                    // 2-3. P1, P2 모두 부정 출발
                     else if(result >= 3) {
                         printf("Both FS Detected!\n");
                         for(int i = 0; i < 4; i++) {
@@ -281,7 +283,7 @@ void Main(void)
                             }
                     }
 
-                    TIM2_Delay(3000); // 3초간 경고 유지
+                    TIM2_Delay(3000);       // 3초간 경고 유지
                 }
 
                 // 카운트다운 직후 차량이 바로 출발하므로 잠시 대기
@@ -297,4 +299,3 @@ void Main(void)
         for (volatile int i = 0; i < 500000; i++);
     }
 }
-
